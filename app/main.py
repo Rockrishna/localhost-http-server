2761109bbeab43fa66bec8cf9a9fbdf81f629a53
Vasant Suresh <41347270+Rockrishna@users.noreply.github.com>
2024-03-21 13:42:22 -0400

    Refactor server code to handle HTTP requests

--- app/main.py
+++ app/main.py
@@ -2,24 +2,25 @@ import socket
 
 def main():
     with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
-
-            while True:
-
-                client_socket, address = server_socket.accept() # Wait for a client
-
-                with client_socket:
-
-                    while True:
-
-                        data = client_socket.recv(1024)
-
-                        if data:
-
-                            break
-
-                    # Send HTTP response to the client
-
-                    client_socket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
+        while True:
+            client_socket, address = server_socket.accept() # Wait for a client
+            with client_socket:
+                while True:
+                    data = client_socket.recv(1024)
+                    if data:
+                        break
+                # Send HTTP response to the client
+                client_socket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
+            
+            if client_socket:
+                 break
+        parsed_data = data.decode()
+        #parse the data
+        get, host, user_agent = parsed_data.split('\r\n')[0], parsed_data.split('\r\n')[1], parsed_data.split('\r\n')[2]
+        if get[4] = '/':
+            client_socket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
+        else:
+            client_socket.send('HTTP/1.1 404 Not Found\r\n\r\n'.encode())
 
     client_socket.close()
 if __name__ == "__main__":
