4f1e720afe5c82b76e151818201f13012a47f274
Vasant Suresh <41347270+Rockrishna@users.noreply.github.com>
2024-03-21 13:33:58 -0400

    Refactor socket receive loop in main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,10 @@ import socket
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, address = server_socket.accept() # wait for client
-    data = client_socket.recv(1024)
+    while True:
+        data = client_socket.recv(1024)
+        if data:
+            break
     #data.decode() to grab data
     server_socket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
 
