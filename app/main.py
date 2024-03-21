b3436b825fa5408b2a1e3416b384886f846f31d9
Vasant Suresh <41347270+Rockrishna@users.noreply.github.com>
2024-03-21 13:37:04 -0400

    Refactor socket handling in main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,25 @@
 import socket
 
 def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    client_socket, address = server_socket.accept() # wait for client
-    while True:
-        data = client_socket.recv(1024)
-        if data:
-            break
-    #data.decode() to grab data
-    server_socket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
+    with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
 
+            while True:
+
+                client_socket, address = server_socket.accept() # Wait for a client
+
+                with client_socket:
+
+                    while True:
+
+                        data = client_socket.recv(1024)
+
+                        if data:
+
+                            break
+
+                    # Send HTTP response to the client
+
+                    client_socket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
 
     client_socket.close()
 if __name__ == "__main__":
