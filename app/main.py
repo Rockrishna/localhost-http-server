afd6e1343609eb911fb3be00e7f76be3204da76f
Vasant Suresh <41347270+Rockrishna@users.noreply.github.com>
2024-03-21 13:32:49 -0400

    test

--- app/main.py
+++ app/main.py
@@ -4,7 +4,10 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, address = server_socket.accept() # wait for client
     data = client_socket.recv(1024)
-    print(data.decode())
+    #data.decode() to grab data
+    server_socket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
 
+
+    client_socket.close()
 if __name__ == "__main__":
     main()
