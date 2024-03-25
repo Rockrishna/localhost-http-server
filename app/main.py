af8d43c9259a025ba41aef255fafcb2251d14e72
Vasant Suresh <41347270+Rockrishna@users.noreply.github.com>
2024-03-25 09:09:47 -0400

    Fix parsing issue in main.py

--- app/main.py
+++ app/main.py
@@ -13,7 +13,8 @@ def main():
                 parsed_data = data.decode()
                 #parse the data
                 get, host, user_agent = parsed_data.split('\r\n')[0], parsed_data.split('\r\n')[1], parsed_data.split('\r\n')[2]
-                if get[4] == r'/':
+                get = get.split(' ')[1] = get.split(' ')[2]
+                if get[0] == r'/':
                     client_socket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
                 else:
                     client_socket.send('HTTP/1.1 404 Not Found\r\n\r\n'.encode())
