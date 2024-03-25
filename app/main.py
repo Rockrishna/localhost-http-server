1dfa6eb4e166f0b44333329978bff01faba57d5c
Vasant Suresh <41347270+Rockrishna@users.noreply.github.com>
2024-03-25 07:36:01 -0400

    test

--- app/main.py
+++ app/main.py
@@ -1,19 +1,20 @@
 import socket
 
 def main():
+    data = False
     with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
         while True:
             client_socket, address = server_socket.accept() # Wait for a client
-            with client_socket:
-                while True:
-                    data = client_socket.recv(1024)
-                    if data:
-                        break
+            if client_socket:
+                with client_socket:
+                    while True:
+                        data = client_socket.recv(1024)
+                        if data:
+                            break
+            if data:
+                break
                 # Send HTTP response to the client
                 #client_socket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
-            
-            if client_socket:
-                 break
         parsed_data = data.decode()
         #parse the data
         get, host, user_agent = parsed_data.split('\r\n')[0], parsed_data.split('\r\n')[1], parsed_data.split('\r\n')[2]
