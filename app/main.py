20f4f8e08ba28ad2266df72348841e09bb412271
Vasant Suresh <41347270+Rockrishna@users.noreply.github.com>
2024-03-25 13:57:36 -0400

    test

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 
 def main():
     with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
@@ -16,7 +17,8 @@ def main():
                 get, host, user_agent = parsed_data.split('\r\n')[0], parsed_data.split('\r\n')[1], parsed_data.split('\r\n')[2]
                 path = get.split(' ')[1]
                 if r"/" == path:
-                    client_socket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
+                    string = "Hello, World!"
+                    client_socket.send(f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n{string}'.encode())
                 else:
                     client_socket.send('HTTP/1.1 404 Not Found\r\n\r\n'.encode())
                 client_socket.close()
